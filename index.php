11f728aa12275cd751560e15119f10d75a3e7921
unknown <mooseheadmoosic@gmail.com>
2012-12-18 10:19:29 +0100

    slot machine

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+<div id='wrapper'>
+	
+	
+    
+</div>
+
+</body>
+</html>
