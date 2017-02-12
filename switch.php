c8e0af47406bde7aff39a03c906fb779ba80e738
junkdeck <fred@mslsr.com>
2017-02-12 16:39:55 +0100

    gfx folder
    who updates a 4-year old school project just to move the graphics into a
    folder?

--- switch.php
+++ switch.php
@@ -1,68 +1,73 @@
-<?php 
+<?php
+$sausage = "<img src='gfx/sausage.png' />";
+$dead = "<img src='gfx/dead.png' />";
+$eye = "<img src='gfx/eye.png' />";
+$splat = "<img src='gfx/splat.png' />";
+$seven = "<img src='gfx/7.png' />";
 switch($num1)
                 {
-                    case "0":
-                    print "<img src='sausage.png' />";
-                    break;
+                  case "0":
+                  print $sausage;
+                  break;
 
-                    case "1":
-                    print "<img src='dead.png' />";
-                    break;
-					                    
-                    case "2":
-                    print "<img src='eye.png' />";
-                    break;
-                    
-                    case "3":
-                    print "<img src='splat.png' />";
-                    break;
-                    
-                    case "4":
-                    print "<img src='7.png' />";
-                    break;
+                  case "1":
+                  print $dead;
+                  break;
+
+                  case "2":
+                  print $eye;
+                  break;
+
+                  case "3":
+                  print $splat;
+                  break;
+
+                  case "4":
+                  print $seven;
+                  break;
                 }
 				switch($num2)
                 {
-                    case "0":
-                    print "<img src='sausage.png' />";
-                    break;
+                  case "0":
+                  print $sausage;
+                  break;
 
-                    case "1":
-                    print "<img src='dead.png' />";
-                    break;
-					                    
-                    case "2":
-                    print "<img src='eye.png' />";
-                    break;
-                    
-                    case "3":
-                    print "<img src='splat.png' />";
-                    break;
-                    
-                    case "4":
-                    print "<img src='7.png' />";
-                    break;
+                  case "1":
+                  print $dead;
+                  break;
+
+                  case "2":
+                  print $eye;
+                  break;
+
+                  case "3":
+                  print $splat;
+                  break;
+
+                  case "4":
+                  print $seven;
+                  break;
                 }
 				switch($num3)
                 {
-                    case "0":
-                    print "<img src='sausage.png' />";
-                    break;
+                  case "0":
+                  print $sausage;
+                  break;
 
-                    case "1":
-                    print "<img src='dead.png' />";
-                    break;
-					                    
-                    case "2":
-                    print "<img src='eye.png' />";
-                    break;
-                    
-                    case "3":
-                    print "<img src='splat.png' />";
-                    break;
-                    
-                    case "4":
-                    print "<img src='7.png' />";
-                    break;
-                }			
+                  case "1":
+                  print $dead;
+                  break;
+
+                  case "2":
+                  print $eye;
+                  break;
+
+                  case "3":
+                  print $splat;
+                  break;
+
+                  case "4":
+                  print $seven;
+                  break;
+                }
 ?>
